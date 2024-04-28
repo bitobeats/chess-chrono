92791fd32b4c3fe64ec8a6d219900e0e9b377bf0
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2024-04-28 00:22:23 -0300

    configuring pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import autoprefixer from "autoprefixer";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,svf,woff,woff2,wasm}"],
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [autoprefixer()],
