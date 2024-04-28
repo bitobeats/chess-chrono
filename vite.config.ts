4126dd936359680a5a851d1543419a1031dd53c2
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2024-04-27 21:21:45 -0300

    fixed mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [solid()],
-})
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+});
