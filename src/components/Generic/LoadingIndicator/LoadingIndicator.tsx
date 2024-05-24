3ab1e898a1491b18602b05ef6e0f27a4f1ca3eee
bitobeats <miseravel.cruller-0o@icloud.com>
2024-05-24 19:12:37 -0300

    fixed LoadingIndicator class passing

--- src/components/Generic/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/Generic/LoadingIndicator/LoadingIndicator.tsx
@@ -2,9 +2,16 @@ import type { VoidComponent, JSX } from "solid-js";
 
 import styles from "./LoadingIndicator.module.scss";
 
+import { splitProps } from "solid-js";
+
 export const LoadingIndicator: VoidComponent<JSX.SvgSVGAttributes<SVGSVGElement>> = (props) => {
+  const [localProps, restProps] = splitProps(props, ["class"]);
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class={styles.loadingIndicator} {...props}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      class={`${styles.loadingIndicator} ${localProps.class}`}
+      {...restProps}>
       <g>
         <circle cx={3} cy={12} r={2}></circle>
         <circle cx={21} cy={12} r={2}></circle>
