c617076a29c1df9bfac6abba07dc9af12e7047fb
bitobeats <miseravel.cruller-0o@icloud.com>
2024-05-24 19:00:52 -0300

    added basic loading indicator

---
+++ src/components/Generic/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,26 @@
+import type { VoidComponent, JSX } from "solid-js";
+
+import styles from "./LoadingIndicator.module.scss";
+
+export const LoadingIndicator: VoidComponent<JSX.SvgSVGAttributes<SVGSVGElement>> = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class={styles.loadingIndicator} {...props}>
+      <g>
+        <circle cx={3} cy={12} r={2}></circle>
+        <circle cx={21} cy={12} r={2}></circle>
+        <circle cx={12} cy={21} r={2}></circle>
+        <circle cx={12} cy={3} r={2}></circle>
+        <circle cx={5.64} cy={5.64} r={2}></circle>
+        <circle cx={18.36} cy={18.36} r={2}></circle>
+        <circle cx={5.64} cy={18.36} r={2}></circle>
+        <circle cx={18.36} cy={5.64} r={2}></circle>
+        <animateTransform
+          attributeName="transform"
+          dur="1.5s"
+          repeatCount="indefinite"
+          type="rotate"
+          values="0 12 12;360 12 12"></animateTransform>
+      </g>
+    </svg>
+  );
+};
