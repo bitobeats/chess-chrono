e77b6244c20c2786b15c5077300e8abf1fe3e079
bitobeats <miseravel.cruller-0o@icloud.com>
2025-10-07 17:30:48 -0300

    Reorganizes lib setup

--- src/libs/libsSetup.ts
+++ src/libs/libsSetup.ts
@@ -5,8 +5,6 @@ import { SettingsManager } from "./settings-manager/SettingsManager";
 import { Database } from "./persister/Database";
 import { AudioPlayer } from "./audio-player/AudioPlayer";
 
-const audioPlayer = new AudioPlayer();
-
 type DatabaseStores = {
   app: {
     settings: Settings;
@@ -14,55 +12,63 @@ type DatabaseStores = {
   };
 };
 
-const db = new Database<DatabaseStores>();
-
-await Promise.all([
-  db.init({
+async function initLibs() {
+  const db = new Database<DatabaseStores>();
+  await db.init({
     name: "chess-chrono",
     version: 1,
     onUpgradeNeeded: (request) => {
       request.result.createObjectStore("app");
     },
-  }),
-  audioPlayer.init(),
-]);
+  });
 
-const settingsManager = new SettingsManager(db.createRecordHandler("app", "settings"));
-await settingsManager.init();
+  const audioPlayer = new AudioPlayer();
+  const settingsManager = new SettingsManager(db.createRecordHandler("app", "settings"));
 
-const chessClockService = new ChessClockService(
-  {
-    countdownFrom: settingsManager.settings.player1.startTime,
-    incrementBy: settingsManager.settings.player1.incrementBy,
-  },
-  {
-    countdownFrom: settingsManager.settings.player2.startTime,
-    incrementBy: settingsManager.settings.player2.incrementBy,
-  }
-);
+  await Promise.all([audioPlayer.init(), settingsManager.init()]);
 
-function updateSettings(newSettings: Readonly<Settings>) {
-  if (
-    chessClockService.player1Config.countdownFrom !== newSettings.player1.startTime ||
-    chessClockService.player1Config.incrementBy !== newSettings.player1.incrementBy ||
-    chessClockService.player2Config.countdownFrom !== newSettings.player2.startTime ||
-    chessClockService.player2Config.incrementBy !== newSettings.player2.incrementBy
-  ) {
-    chessClockService.reset();
-  }
-
-  chessClockService.player1Config.countdownFrom = newSettings.player1.startTime;
-  chessClockService.player1Config.incrementBy = newSettings.player1.incrementBy;
-  chessClockService.player2Config.countdownFrom = newSettings.player2.startTime;
-  chessClockService.player2Config.incrementBy = newSettings.player2.incrementBy;
-
-  chessClockService.dispatchEvent(
-    "playerconfigchange",
-    chessClockService.player1Config,
-    chessClockService.player2Config
+  const chessClockService = new ChessClockService(
+    {
+      countdownFrom: settingsManager.settings.player1.startTime,
+      incrementBy: settingsManager.settings.player1.incrementBy,
+    },
+    {
+      countdownFrom: settingsManager.settings.player2.startTime,
+      incrementBy: settingsManager.settings.player2.incrementBy,
+    }
   );
+
+  return { settingsManager, chessClockService, audioPlayer };
 }
 
-settingsManager.addEventListener("settingssaved", updateSettings);
+function wireLibs(chessClockService: ChessClockService, settingsManager: SettingsManager) {
+  function updateSettings(newSettings: Readonly<Settings>) {
+    if (
+      chessClockService.player1Config.countdownFrom !== newSettings.player1.startTime ||
+      chessClockService.player1Config.incrementBy !== newSettings.player1.incrementBy ||
+      chessClockService.player2Config.countdownFrom !== newSettings.player2.startTime ||
+      chessClockService.player2Config.incrementBy !== newSettings.player2.incrementBy
+    ) {
+      chessClockService.reset();
+    }
+
+    chessClockService.player1Config.countdownFrom = newSettings.player1.startTime;
+    chessClockService.player1Config.incrementBy = newSettings.player1.incrementBy;
+    chessClockService.player2Config.countdownFrom = newSettings.player2.startTime;
+    chessClockService.player2Config.incrementBy = newSettings.player2.incrementBy;
+
+    chessClockService.dispatchEvent(
+      "playerconfigchange",
+      chessClockService.player1Config,
+      chessClockService.player2Config
+    );
+  }
+
+  settingsManager.addEventListener("settingssaved", updateSettings);
+}
+
+const { audioPlayer, chessClockService, settingsManager } = await initLibs();
+
+wireLibs(chessClockService, settingsManager);
 
 export { settingsManager, chessClockService, audioPlayer };
